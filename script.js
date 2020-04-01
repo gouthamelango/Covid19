48f5b0df069a9d4c1394756b34f444e63cb16362
Goutham Elango <goutham4elango@gmail.com>
2020-04-01 11:39:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch('https://corona.lmao.ninja/all')
     .then(data => generateHtml(data))
 
 const generateHtml = (data) =>{
-    console.log(data);
+   // console.log(data);
     document.querySelector('.total_cases').innerHTML = data.cases
     document.querySelector('.total_recovered').innerHTML = data.recovered
     document.querySelector('.total_deaths').innerHTML = data.deaths
@@ -15,7 +15,7 @@ fetch('https://thevirustracker.com/free-api?global=stats')
     .then(data => generateHtml2(data))
 
 const generateHtml2 = (data) =>{
-    console.log(data);
+    //console.log(data);
     document.querySelector('.total_new_cases_today').innerHTML = data.results[0].total_new_cases_today
     document.querySelector('.total_new_deaths_today').innerHTML = data.results[0].total_new_deaths_today
 }
@@ -25,7 +25,7 @@ fetch('https://corona.lmao.ninja/countries?sort=country')
     .then(data =>generateHtml1(data))
 
     const generateHtml1 = (data) =>{
-        console.log(data); 
+       // console.log(data); 
         document.querySelector('.incasesoff').innerHTML = "Official: <b> "+data[114].cases+"<b>"
         document.querySelector('.inrecoveredoff').innerHTML = "Official: <b> "+data[114].recovered+"<b>"
         document.querySelector('.indeathsoff').innerHTML = "Official: <b> "+data[114].deaths+"<b>"
@@ -38,7 +38,7 @@ fetch('https://corona.lmao.ninja/countries?sort=country')
     .then(data =>generateHtml3(data))
 
     const generateHtml3 = (data) =>{
-        console.log(data); 
+        //console.log(data); 
        document.querySelector('.incasesun').innerHTML = "Unofficial: <b> "+data.data.total.confirmed+"<b>"
         document.querySelector('.inrecoveredun').innerHTML = "Unofficial: <b> "+data.data.total.recovered+"<b>"
         document.querySelector('.indeathsun').innerHTML = "Unofficial: <b> "+data.data.total.deaths+"<b>"
